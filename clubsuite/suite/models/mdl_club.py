430009937bf64c91b1932e0a42aa4cdbca476af5
PKpinkuro <jiw272@ucsd.edu>
2017-02-19 15:11:24 -0800

    #23 fixed

--- clubsuite/suite/models/mdl_club.py
+++ clubsuite/suite/models/mdl_club.py
@@ -2,5 +2,10 @@ from django.db import models
 
 class Club(models.Model):
    club_name = models.CharField(max_length=50)
-   club_type = models.CharField(max_length=20)
+
+   C_CHOICES = (
+       ('PUB','Public'),
+       ('PRI','Private')
+   )
+   club_type = models.CharField(max_length=3,choices=C_CHOICES,default='PUB')
    club_description = models.CharField(max_length=1000)
