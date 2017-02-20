ddeb8404570e69ba8639b03895dcc2ccc1531c48
PKpinkuro <jiw272@ucsd.edu>
2017-02-19 16:37:36 -0800

    str function fixed

--- clubsuite/suite/models/mdl_user.py
+++ clubsuite/suite/models/mdl_user.py
@@ -47,7 +47,7 @@ class User(AbstractBaseUser, PermissionsMixin):
     def get_short_name(self):
         return self.first_name
     def __str__(self):
-        return get_full_name(self)
+        return self.first_name+' '+self.last_name
 
 class Account(models.Model):
     user = models.OneToOneField(
--- clubsuite/suite/models/qry_clubmembers.py
+++ clubsuite/suite/models/qry_clubmembers.py
@@ -1,5 +1,5 @@
 from .mdl_role import *
 
-    def qry_clubmembers(in_cid):
-        club_members = Role.objects.filter(cid=in_cid)
-        return club_members
+def qry_clubmembers(in_cid):
+    club_members = Role.objects.filter(cid=in_cid)
+    return club_members
