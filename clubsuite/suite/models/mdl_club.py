faff5b110874b494539a6fa535b0644da19933f1
Kevin Zavier <kzavier@ucsd.edu>
2017-02-19 16:13:53 -0800

    created a default image

--- clubsuite/suite/models/mdl_club.py
+++ clubsuite/suite/models/mdl_club.py
@@ -4,7 +4,7 @@ from django.utils import timezone
 class Club(models.Model):
    club_name = models.CharField(max_length=50)
    club_type = models.CharField(max_length=20)
-   image = models.ImageField(upload_to='media/', default="/static/images/default.jpg")
+   image = models.ImageField(upload_to='media/', default="/media/default.jpg")
    first_seen = models.DateTimeField(editable=False, blank=True, null=True)
    last_seen = models.DateTimeField(blank=True, null=True)
    club_description = models.TextField()
