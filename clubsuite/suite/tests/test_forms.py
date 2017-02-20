08f01609279afa123d645fcde91daa0f86d38f7e
U-JACKY-PC\jcjac <wec053@ucsd.edu>
2017-02-19 21:52:48 -0800

    test_forms.py errors fixed

--- clubsuite/suite/tests/test_forms.py
+++ clubsuite/suite/tests/test_forms.py
@@ -1,24 +1,28 @@
 from django.test import TestCase
 from suite.forms import RegistrationForm
 from django.contrib.auth import get_user_model
-from django.test import Client
+from django.contrib.auth.hashers import check_password
 # Create your tests here.
 #
 
 class RegistrationFormTestCase(TestCase):
-    def setUp(self):
-        User=get_user_model().objects.create(email="test@test.com",password1="pasdd",password2="pasdd")
+    #def setUp(self):
+        #User=get_user_model().objects.create(email="test@test.com",password1="pasdd",password2="pasdd")
         #RegistrationForm.objects.create(email="test@test.com",password1="pass",password2="pass")
 
 
     def test_valid_data(self):
         form=RegistrationForm(data={'email':"test@test.com",
-                                'password1':"pass",
-                                'password2':"pass"})
+                                'password1':"clubsuite",
+                                'password2':"clubsuite"})
+                                
+        
+        #print(form.errors)#debug
+        #self.assertTrue(form.fields['email']=="test@test.com")
         self.assertTrue(form.is_valid())
-        '''
-        registration=form.save()
-
-        self.assertTrue(form.password1=="pass")
-        self.assertTrue(form.password2=="pass")
-        '''
+        user=form.save()
+        
+        #print(user.email) #debug
+        self.assertTrue(user.email=="test@test.com")
+        self.assertTrue(check_password("clubsuite",user.password))
+        
