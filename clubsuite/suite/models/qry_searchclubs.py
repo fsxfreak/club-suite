cd341aeb5a1aceff00d2b3485c3c3a0c5cbe16bd
Leon Cheung <leonjcheung@gmail.com>
2017-02-22 11:43:00 -0800

    club search working

--- clubsuite/suite/models/qry_searchclubs.py
+++ clubsuite/suite/models/qry_searchclubs.py
@@ -2,7 +2,7 @@ from .mdl_club import *
 from django.db.models import Q
 
 def qry_searchclubs(keyword):
-    r=Club.objects.get(
+    r=Club.objects.filter(
         Q(club_name__contains=keyword) |
         Q(club_description__contains=keyword)
     )
