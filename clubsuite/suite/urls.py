6a012dbf70dc1580694a352ef288c38b72e47bd3
Leon Cheung <leonjcheung@gmail.com>
2017-02-23 12:12:42 -0800

    roster page now shows all members of a club, with Role model. club create automtaically sets the user as owner. club view has link to club roster.

--- clubsuite/suite/urls.py
+++ clubsuite/suite/urls.py
@@ -15,7 +15,7 @@ urlpatterns = [
     url(r'^dashboard', views.Dashboard.as_view(), name='dashboard'),
     url(r'^club/create', views.ClubCreate.as_view(), name='club_create'),
     url(r'^club/search', views.ClubSearch.as_view(), name='club_search'),
-    url(r'^club/roster', views.ClubRoster.as_view(), name='club_roster'),
+    url(r'^club/(?P<club_id>[0-9]+)/roster', views.ClubRoster.as_view(), name='club_roster'),
     url(r'^club/(?P<club_id>[0-9]+)/$', views.ClubView.as_view(), name='club_view'),
     url(r'^account', views.Account.as_view(), name='account'),
 
