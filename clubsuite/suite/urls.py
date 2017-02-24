b1629320bccdbf66a81ea26c7dcee616d040a673
Leon Cheung <fsxfreak@users.noreply.github.com>
2017-02-23 21:25:56 -0800

    fix merge conflicts.

--- clubsuite/suite/urls.py
+++ clubsuite/suite/urls.py
@@ -17,12 +17,8 @@ urlpatterns = [
     url(r'^club/search', views.ClubSearch.as_view(), name='club_search'),
     url(r'^club/(?P<club_id>[0-9]+)/roster', views.ClubRoster.as_view(), name='club_roster'),
     url(r'^club/(?P<club_id>[0-9]+)/$', views.ClubView.as_view(), name='club_view'),
-<<<<<<< HEAD
-    url(r'^account', views.Account.edit_profile, name='edit_profile'),
-=======
     url(r'^club/(?P<club_id>[0-9]+)/join$', views.ClubJoin.as_view(), name='club_join'),
-    url(r'^account', views.Account.as_view(), name='account'),
->>>>>>> b31a70de3bf670a799f601dfbce4ddbb5cef39ac
+    url(r'^account', views.Account.edit_profile, name='edit_profile'),
 
     #password reset
 
