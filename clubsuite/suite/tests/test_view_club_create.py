0c9d86c9cbfaa2eb69892d7bcb90b8097738f436
U-JACKY-PC\jcjac <wec053@ucsd.edu>
2017-02-23 22:18:38 -0800

    test_view_club_create needed client logged in

--- clubsuite/suite/tests/test_view_club_create.py
+++ clubsuite/suite/tests/test_view_club_create.py
@@ -1,4 +1,5 @@
 from django.test import TestCase
+from django.contrib.auth import get_user_model
 from django.test import Client
 from suite.views import ClubCreate
 from django.urls import reverse
@@ -6,12 +7,13 @@ from django.urls import reverse
 class View_Club_Create_TestCase(TestCase):
     def setUp(self):
         self.client = Client()
-
+        #client needs to be logged in
+        self.client.force_login(get_user_model().objects.get_or_create(first_name='testuser')[0])
 
     def test_get(self):
         response = self.client.get(reverse('suite:club_create'))
-        #self.assertEqual(response.status_code,200)
-        self.assertRedirects(response, "/?next="+reverse('suite:club_create'), 302,200)
+        self.assertEqual(response.status_code,200)
+        #self.assertRedirects(response, "/?next="+reverse('suite:club_create'), 302,200)
 
     def test_post(self):
         data = {
@@ -20,7 +22,7 @@ class View_Club_Create_TestCase(TestCase):
                 'club_description':"Pretty cool"
                 }
         response = self.client.post(reverse('suite:club_create'),data, follow=True)
-        self.assertRedirects(response,"/?next="+reverse('suite:dashboard'))
+        self.assertRedirects(response,reverse('suite:dashboard'))
 
     def test_post_invalid(self):
         data = {
