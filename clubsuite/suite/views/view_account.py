a89b0de0c7e5210a3ecdb39e905d7ed5f5bb52c1
Leon Cheung <leonjcheung@gmail.com>
2017-02-24 16:30:33 -0800

    some fixes

--- clubsuite/suite/views/view_account.py
+++ clubsuite/suite/views/view_account.py
@@ -2,31 +2,24 @@ from django.shortcuts import render, redirect
 from suite.forms import RegistrationForm,EditProfileForm
 from suite.models import User
 from django.contrib.auth.forms import UserChangeForm, PasswordChangeForm
+from django.contrib.auth import update_session_auth_hash
 
 class Account:
-    #template_name = 'account.html'
+  def edit_profile(request):
+    if request.POST:
+      if 'details' in request.POST:
+        form = EditProfileForm(request.POST, instance=request.user)
+        form.save()
 
-    def edit_profile(request):
-        if request.POST:
-            print(request.POST)
-            if 'details' in request.POST:
-                form = EditProfileForm(request.POST, instance=request.user)
-                form.save()
-            elif 'password' in request.POST:
-                form = PasswordChangeForm(request.POST)
-                form.save()
-            return redirect('/account')
-        else:
-            form = EditProfileForm(instance=request.user)
-            form2 = PasswordChangeForm(user=request.user)
-            args = {'form': form, 'form2': form2 }
-            return render(request, 'account.html',args)
+      elif 'password' in request.POST:
+        form = PasswordChangeForm(request.POST)
+        if form.is_valid():
+          print('form valid')
+          form.save()
+          update_session_auth_hash(request, form.user)
 
-'''
-            if form.is_valid():
-                form.save()
-                return redirect('/account')
-            else:
-                print(form)
-                return redirect('/dashboard')
-'''
+    form = EditProfileForm(instance=request.user)
+    form2 = PasswordChangeForm(user=request.user)
+    args = {'form': form, 'form2': form2 }
+    return render(request, 'account.html', args)
+
