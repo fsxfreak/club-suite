824c7dae2191c7b9a0aa98b8809eabd51716cbc8
Leon Cheung <fsxfreak@users.noreply.github.com>
2017-02-26 14:08:52 -0800

    Database to master merge, for permissions (#77)
    * qry_* files deleted
    
    * model/__init__.py fixed
    
    * object field added

--- clubsuite/suite/models/mdl_role.py
+++ clubsuite/suite/models/mdl_role.py
@@ -1,12 +1,29 @@
 from django.db import models
 
+class RoleManager(models.Manager):
+    def qry_clubmembers(in_cid):
+        club_users = Role.objects.filter(cid=in_cid)
+        club_members = club_users.exclude(title='passerby')
+        return club_members
+
+    # gives list of unresolved requests for joining this club
+    def qry_requestlist(in_cid):
+        club_record = Role.objects.filter(cid=in_cid)
+        club_request = club_record.filter(title='join')
+        return club_request
+
+    # gives list of unresolved requests for promotion in this club
+    def qry_promotelist(in_cid):
+        club_record = Role.objects.filter(cid=in_cid)
+        club_promote = club_record.filter(title='promote')
+        return club_promote
+
 class Role(models.Model):
     # cid refers to the club object, not the club id
     cid = models.ForeignKey(
         'Club',
         on_delete=models.CASCADE
     )
-   
     # uid refers to the user object, not the user id
     uid = models.ForeignKey(
         'User',
@@ -28,6 +45,8 @@ class Role(models.Model):
         choices=R_CHOICES,
         default=PASSERBY
     )
+
+    objects = RoleManager()
 
     def __str__(self):
         s='User '+str(self.uid)+' is '+str(self.title)+\
