f0aa850c18a6e6d5e1f442c699bfdcf8e4f19717
PKpinkuro <jiw272@ucsd.edu>
2017-02-26 15:10:43 -0800

    recover

--- clubsuite/suite/models/mdl_user.py
+++ clubsuite/suite/models/mdl_user.py
@@ -72,6 +72,8 @@ class User(AbstractBaseUser, PermissionsMixin):
         from guardian.models import UserObjectPermission
         UserObjectPermission.objects.assign_perm(i_codename,self,obj=groupob)
 
+    def get_clubs(self):
+        
 class Account(models.Model):
     user = models.OneToOneField(
        User, 
