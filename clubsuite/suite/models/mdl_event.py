0aa726e3f54a6ed491f682179958d76ba67bbb7d
Shijie <florencefsj@gmail.com>
2017-02-26 22:56:11 -0800

    #69

--- clubsuite/suite/models/mdl_event.py
+++ clubsuite/suite/models/mdl_event.py
@@ -1,5 +1,14 @@
 from django.db import models
 
+class EventManager(models.Manager):
+
+   def get_upcoming_events(in_cid):
+      upcoming_events=Event.objects.filter(
+                        cid=in_cid,
+                        start_time__gte=datetime.date.today()
+                        ).order_by('-start_time')
+      return upcoming_events
+
 class Event(models.Model):
    cid = models.ForeignKey(
       'Club',
@@ -14,6 +23,8 @@ class Event(models.Model):
    event_cost = models.DecimalField(max_digits=10, decimal_places=2,default=0)
    accessibility = models.BooleanField(default=True) #True=public, False=private
    required = models.BooleanField(default=False)
+
+   objects = EventManager()
 
    def __str__(self):
        return self.event_name
--- clubsuite/suite/models/mdl_eventsignin.py
+++ clubsuite/suite/models/mdl_eventsignin.py
@@ -2,8 +2,15 @@ from django.db import models
 
 class EventSignInManager(models.Manager):
     def get_attended_events(in_uid,in_cid):
-        attended_events=EventSignIn.object.filter(cid=in_cid,\
-                        uid=in_uid)
+        attended_eid=EventSignIn.objects.values_list('eid',
+                        flat=True).filter(cid=in_cid,uid=in_uid)
+        attended_events=Event.objects.filter(id__in=set(attended_eid)).order_by('-start_time')
+        return attended_events
+
+    def get_all_attended_events(in_uid):
+        attended_eid=EventSignIn.objects.values_list('eid',
+                        flat=True).filter(uid=in_uid)
+        attended_events=Event.objects.filter(id__in=set(attended_eid)).order_by('-start_time')
         return attended_events
 
 class EventSignIn(models.Model):
@@ -20,6 +27,8 @@ class EventSignIn(models.Model):
         'User',
         on_delete = models.CASCADE,
     )
+
+    objects = EventSignInManager()
 
     def __str__(self):
         s='User '+str(self.uid)+' Event '+str(self.eid)+\
