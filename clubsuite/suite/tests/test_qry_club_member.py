3f7e8360ca611cc43fb56c598f8a5e483fae418e
U-JACKY-PC\jcjac <wec053@ucsd.edu>
2017-03-01 23:57:42 -0800

    used user_model to create users instead of RegistrationForm

--- clubsuite/suite/tests/test_qry_club_member.py
+++ clubsuite/suite/tests/test_qry_club_member.py
@@ -1,25 +1,31 @@
 from django.test import TestCase
 from suite.models import Club
-from suite.forms import RegistrationForm
 from suite.models import ClubManager
+from django.contrib.auth import get_user_model
 
 class ClubMemberQueryTestCase(TestCase):
     def setUp(self):
 
+        #create club owner
+        self.owner=get_user_model().objects.create(first_name="Owner",last_name="McPerson",email="test@test.com")
+        self.owner.set_password("clubsuite")
+        self.owner.save()
+
+
         #Create club
         self.club=Club.objects.create(club_name="club",club_type="PUB",
                                     club_description="a club")
-        self.club2=Club.objects.create(club_name="club2",club_type="PUB",
-                                    club_description="another club")
-        self.club3=Club.objects.create(club_name="club3",club_type="PUB",club_description="empty club")
-
-        #create club owner
-        form=RegistrationForm(data={'email':"test@test.com",'password1':"clubsuite",'password2':"clubsuite",'first_name':"Owner",'last_name':"McPerson"})
-        self.assertTrue(form.is_valid())
-        self.owner=form.save()
-        self.owner.save()
         self.club._create_permissions()
         self.club._set_owner(self.owner)
+
+        #Create club2
+        self.club2=Club.objects.create(club_name="club2",club_type="PUB",
+                                    club_description="another club")
+        self.club2._create_permissions()
+        self.club2._set_owner(self.owner)
+
+        #Create empty club
+        self.club3=Club.objects.create(club_name="club3",club_type="PUB",club_description="empty club")
 
 
         self.clubmanager=ClubManager()
@@ -27,11 +33,12 @@ class ClubMemberQueryTestCase(TestCase):
 
         #add users to club
         for i in range(1,20):
-            form=RegistrationForm(data={'email':"test"+str(i)+"@test.com",'password1':"clubsuite",'password2':"clubsuite",'first_name':"Person"+str(i),'last_name':"McPerson"})
-            self.assertTrue(form.is_valid())
-            user=form.save()
+            #create users
+            user=get_user_model().objects.create(first_name="Person"+str(i),last_name="McPerson",email="test"+str(i)+"@test.com")
+            user.set_password("clubsuite")
             user.save()
-            # checks actor's permission (first parameter)
+
+            # add user to club
             self.club.add_member(self.owner,user)
             self.user_list.append(user)
 
