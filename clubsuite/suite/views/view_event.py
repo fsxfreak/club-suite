095752ba08c228bf38357714203050694e23fa5a
Kevin Zavier <kzavier@ucsd.edu>
2017-03-03 00:40:13 -0800

    fix merge

---
+++ clubsuite/suite/views/view_event.py
@@ -0,0 +1,21 @@
+from django.shortcuts import render, get_object_or_404
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.views.generic import View
+from suite.models import Event
+
+class Event(TemplateView):
+    template_name = 'event.html'
+    def get(self, request, club_id, event_id, *args, **kwargs):
+       club = get_object_or_404(Club, pk=club_id)
+       event = get_object_or_404(Event, pk=event_id)
+       mems = club.members.all()
+
+       members = []
+       for mem in mems:
+         member = {'user': mem, 'group': None } # TODO group
+         print(member)
+         members.append(member)
+
+       print(members)
+
+       return render(request, self.template_name, {'club': club, 'members' : members, 'event': event})
--- clubsuite/suite/views/view_event_create.py
+++ clubsuite/suite/views/view_event_create.py
@@ -3,3 +3,10 @@ from django.views.generic import TemplateView
 
 class EventCreate(TemplateView):
     template_name = 'event_create.html'
+
+
+    def post(self, request, club_id):
+      form = self.form_class(request.POST)
+      club = get_object_or_404(Club, pk=club_id)
+      if form.is_valid():
+        event = form.save(club, commit=True)
