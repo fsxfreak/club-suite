e851d11164159fc60d4b61e6dc2f38995b6bd1ab
Aparna Rangamani <arangama@ucsd.edu>
2017-03-03 01:42:53 -0800

    #21 fixed club create- only adding a club photo needs aesthetic improvement, #109 moved event_create from club view page, #47 updated event_create with new fields and confirmed it is working, am waiting for merge from database to get the updated time/date fields

--- clubsuite/suite/models/mdl_event.py
+++ clubsuite/suite/models/mdl_event.py
@@ -12,7 +12,8 @@ class Event(models.Model):
    start_time = models.DateTimeField(default=datetime.now, blank=True)
    end_time = models.DateTimeField(default=datetime.now, blank=True)
    event_location = models.CharField(max_length=100)
-   event_description = models.CharField(max_length=1000)
+   #event_description = models.CharField(max_length=1000)
+   event_description = models.TextField()
    event_cost = models.DecimalField(max_digits=10, decimal_places=2,default=0)
    accessibility = models.BooleanField(default=True) #True=public, False=private
    required = models.BooleanField(default=False)
