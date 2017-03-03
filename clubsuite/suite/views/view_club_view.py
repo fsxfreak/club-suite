65a1d4de99aec9ed919a08bb6c03d10f2e11b2fd
Leon Cheung <leonjcheung@gmail.com>
2017-03-03 03:37:38 -0800

    fixed event create

--- clubsuite/suite/views/view_club_view.py
+++ clubsuite/suite/views/view_club_view.py
@@ -2,34 +2,16 @@ from django.shortcuts import render, get_object_or_404
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.http import HttpResponse, HttpResponseRedirect
 from django.urls import reverse
-from django.views.generic import TemplateView
-from suite.models import Club
-from suite.forms import EventCreateForm
-from suite.models import Event
+from django.views.generic import View
 
+from suite.models import Club, Event
 from . import models
 
-
-class ClubView(LoginRequiredMixin, TemplateView):
-    form_class = EventCreateForm
+class ClubView(LoginRequiredMixin, View):
     template_name = 'dashboard/club_view.html'
 
     def get(self, request, club_id):
         club = get_object_or_404(Club, pk=club_id)
-        form = self.form_class
         events = Event.objects.filter(cid=club_id)
 
-        return render(request, self.template_name, {'club': club, 'form': form, 'events': events})
-
-    def post(self, request, club_id):
-      form = self.form_class(request.POST)
-      club = get_object_or_404(Club, pk=club_id)
-      if form.is_valid():
-        event = form.save(club, commit=True)
-        #role = Role(title='O', cid=club, uid=request.user).save()
-
-
-
-      # create form with request
-      events = Event.objects.filter(cid=club_id)
-      return render(request, self.template_name, {'club': club, 'form' : form, 'events': events})
+        return render(request, self.template_name, {'club': club, 'events': events})
--- clubsuite/suite/views/view_event_create.py
+++ clubsuite/suite/views/view_event_create.py
@@ -1,19 +1,25 @@
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseRedirect
 from django.views.generic import TemplateView
 from django.shortcuts import render, get_object_or_404
+from django.urls import reverse
 
 from suite.models import Club
+from suite.forms import EventCreateForm
 
 class EventCreate(TemplateView):
+    form_class = EventCreateForm
     template_name = 'event_create.html'
 
     def get(self, request, club_id):
       club = get_object_or_404(Club, pk=club_id)
-      return render(request, self.template_name, { 'club' : club })
+      form = self.form_class
+      return render(request, self.template_name, { 'club' : club, 'form' : form})
 
     def post(self, request, club_id):
       form = self.form_class(request.POST)
       club = get_object_or_404(Club, pk=club_id)
       if form.is_valid():
         event = form.save(club, commit=True)
+
+      return HttpResponseRedirect(reverse('suite:club_view', args=[club_id]))
 
