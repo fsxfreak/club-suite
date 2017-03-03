e7354a424fd390765a238842740e3213ad616778
Leon Cheung <leonjcheung@gmail.com>
2017-03-02 21:57:21 -0800

    club emails base functionality

--- clubsuite/suite/views/view_club_emails.py
+++ clubsuite/suite/views/view_club_emails.py
@@ -7,29 +7,24 @@ from suite.models import Club
 class ClubEmails(LoginRequiredMixin, View):
   template_name = 'dashboard/club_emails.html'
 
+  def get_members(self, club):
+    members = []
+    for member in club.members.all():
+      members.append( {'user' : member, 'group' : None })
+
+    return members
+
   def get(self, request, club_id):
     club = get_object_or_404(Club, pk=club_id)
-    members = []
-    for member in club.members:
-      members.append( {
-        'full_name' : None
-        , 'email' : None
-        , 'group' : None
-        }
-      )
-
-      return render(request, self.template_name, {'members': members})
+    members = self.get_members(club)
+    print(members)
+  
+    return render(request, self.template_name, {'members': members})
 
   def post(self, request, club_id):
     club = get_object_or_404(Club, pk=club_id)
     members = []
     # TODO instead of members return officers
-    for member in club.members:
-      members.append( {
-        'full_name' : None
-        , 'email' : None
-        , 'group' : None
-        }
-      )
+    members = self.get_members(club)
 
     return render(request, self.template_name, {'members': members})
