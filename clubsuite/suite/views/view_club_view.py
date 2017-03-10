f3fbdf2ea92abacc6a2e9ead4d014526bd4cac0c
Shijie <florencefsj@gmail.com>
2017-03-09 18:48:47 -0800

    conflix view

--- clubsuite/suite/views/view_club_view.py
+++ clubsuite/suite/views/view_club_view.py
@@ -13,17 +13,8 @@ class ClubView(LoginRequiredMixin, View):
 
     def get(self, request, club_id):
         club = get_object_or_404(Club, pk=club_id)
-<<<<<<< HEAD
 
         events = Event.objects.get_upcoming_events(club)
         signedInEvents = EventSignIn.objects.get_attended_events(request.user, club)
-=======
-        events = Event.objects.filter(cid=club_id).order_by('start_date')
-        sign_ins = EventSignIn.objects.filter(cid=club_id, uid=request.user.id)
-        signedInEvents = [sign_in.eid for sign_in in sign_ins ]
-        signedInEvents = sorted(signedInEvents, key=lambda x: x.start_date, reverse=True)
-        #if request[saved]:
-        #    messages.add_message(request, messages.INFO, 'Made a club')
->>>>>>> ee496c6c0e54608abc687724c711e447ecc92bc2
 
         return render(request, self.template_name, {'club': club, 'events': events, 'signedInEvents': signedInEvents})
