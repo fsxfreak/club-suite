ee496c6c0e54608abc687724c711e447ecc92bc2
Leon Cheung <leonjcheung@gmail.com>
2017-03-09 18:18:17 -0800

    complete #221

--- clubsuite/suite/urls.py
+++ clubsuite/suite/urls.py
@@ -23,6 +23,7 @@ urlpatterns = [
     url(r'^club/(?P<club_id>[0-9]+)/join$', views.ClubJoin.as_view(), name='club_join'),
     url(r'^club/(?P<club_id>[0-9]+)/emails$', views.ClubEmails.as_view(), name='club_emails'),
     url(r'^club/(?P<club_id>[0-9]+)/join/handle_requests$', views.HandleJoinRequest.as_view(), name='handle_join_request'),
+    url(r'^club/(?P<club_id>[0-9]+)/edit$', views.ClubEdit.as_view(), name='club_edit'),
     url(r'^club/(?P<club_id>[0-9]+)/budget$', views.Budget.as_view(), name='budget'),
     url(r'^account', views.Account.as_view(), name='edit_profile'),
     url(r'^admin/', admin.site.urls),
