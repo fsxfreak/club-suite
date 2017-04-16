312b5f41e7ff3f9dd332332f74d77c0667a04404
Kevin Zavier <kzavier@ucsd.edu>
2017-04-15 18:48:12 -0700

    even better

--- clubsuite/clubsuite/settings.py
+++ clubsuite/clubsuite/settings.py
@@ -31,7 +31,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['kevinzavier.pythonanywhere.com']
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
